00b943989322555470751d0bd1019b67d7ea8b4a
zhuhj <zhuhjtcx@qq.com>
2018-09-09 03:19:00 -0700

    fsfs

--- demo13-tornado1.py
+++ demo13-tornado1.py
@@ -2,6 +2,13 @@
 import tornado.ioloop
 import tornado.web
 import os
+import re
+from mongo import db
+
+
+def getLabs(str):
+    r = re.compile(ur'@([\u4E00~\u9FA5\w~]+)')
+    return r.findall(str)
 
 
 class BassHandler(tornado.web.RequestHandler):
@@ -20,18 +27,36 @@ class WeiboHandler(BassHandler):
         return self.render('weibo_add.html')
     def post(self):
         result = self.get_argument("content",None)
+        con = getLabs(result)
+        print(con)
+        db.logs.insert({'content':result,'user':self.get_current_user()})
         self.write(result)
 class LoginHandler(BassHandler):
     def get(self):
-        self.write('<html><body><form action="/login" method="post">'
-                   'Name:<input type="text" name="name">'
-                   '<input type="submit" value="sign in">'
-                   '</form></body></html>')
+        self.render('login.html')
     def post(self):
         self.set_secure_cookie("user",self.get_argument('name'))
         self.redirect('/')
 
-
+class RegisterHandler(BassHandler):
+    def get(self):
+        return self.render('user_add.html')
+    def post(self):
+        username = self.get_argument('username')
+        userpwd = self.get_argument('userpwd')
+        if username == '' or userpwd == '':
+            self.write('username or pwd is empty')
+        if db.users.count({"username":username})>0:
+            self.write('this count had registered')
+        self.set_secure_cookie("user",username)
+        db.users.insert({"username":username,"userpwd":userpwd})
+        self.redirect('/userinfo')
+class UserinfoHandler(BassHandler):
+    @tornado.web.authenticated
+    def get(self):
+        user = self.get_current_user()
+        return self.render('userinfo.html',**{"user":user})
+            
 settings={
     'cookie_secret':'lll/vo=',
     'login_url':'/login',
@@ -41,6 +66,8 @@ settings={
 application = tornado.web.Application([
     (r'/',MainHandler),
     (r'/login',LoginHandler),
+    (r'/register',RegisterHandler),
+    (r'/userinfo',UserinfoHandler),
     (r'/weibo/add',WeiboHandler),
     ],**settings)
 if __name__ == "__main__":
